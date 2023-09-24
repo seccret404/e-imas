52907e00e51ff7dae33c6f17a8a71653e8ea5821
@rivaelsagala901 <rivaelsagala901@gmail.com>
2023-09-24 20:04:17 +0700

    update

--- resources/views/admin/akademik/index.blade.php
+++ resources/views/admin/akademik/index.blade.php
@@ -152,9 +152,6 @@
                         </div>
                     </div>
                 </div>
-
-
-
                 <div class="row">
                     <div class="col-12">
                         <div class="mb-3">
